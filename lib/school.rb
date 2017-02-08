7b4e7d059283b1bf36a7cbafca9c7171b40a1954
dison ruan <dison.ruan@yahoo.com>
2017-02-08 16:28:25 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if roster[grade] == nil
+      @roster[grade] = [student]
+    else
+      @roster[grade] << student
+    end
+
+    def grade(num)
+      @roster[num]
+    end
+  end
+
+  def sort
+    @roster.map {|k, v| v.sort!}
+    @roster.sort.to_h
+  end
+
+
+end
